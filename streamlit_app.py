b51fe8e1f9c588e53e62f779abdec797b97a91fe
mandaaabilaa <mandaaurfv@gmail.com>
2025-05-20 08:02:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title(" selamat datang di website mandaabilaa ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration."
 ) 
 st.image ("0e00fdc34a6c8bb474a27e5e43c96744.jpg" width=200)
